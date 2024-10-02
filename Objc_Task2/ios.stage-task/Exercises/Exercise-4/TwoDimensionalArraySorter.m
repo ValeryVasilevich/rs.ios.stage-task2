6747c7bdee0605e348729ee0d71dd5aa815971e6
ValeryVasilevich <>
2024-10-02 18:28:01 +0200

    Impl: exercise 4

--- Objc_Task2/ios.stage-task/Exercises/Exercise-4/TwoDimensionalArraySorter.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-4/TwoDimensionalArraySorter.m
@@ -3,6 +3,37 @@
 @implementation TwoDimensionalArraySorter
 
 - (NSArray *)twoDimensionalSort:(NSArray<NSArray *> *)array {
+    if (![array.firstObject isKindOfClass:[NSArray class]]) {
+        return @[];
+    }
+
+    NSMutableArray *numbers = [NSMutableArray array];
+    NSMutableArray *strings = [NSMutableArray array];
+
+    for (NSArray *subarray in array) {
+        for (id element in subarray) {
+            if ([element isKindOfClass:[NSNumber class]]) {
+                [numbers addObject:element];
+            } else if ([element isKindOfClass:[NSString class]]) {
+                [strings addObject:element];
+            }
+        }
+    }
+
+    if (numbers.count > 0 && strings.count == 0) {
+        [numbers sortUsingSelector:@selector(compare:)];
+        return numbers;
+    } else if (strings.count > 0 && numbers.count == 0) {
+        [strings sortUsingSelector:@selector(compare:)];
+        return strings;
+    } else if (numbers.count > 0 && strings.count > 0) {
+        [numbers sortUsingSelector:@selector(compare:)];
+        [strings sortUsingComparator:^NSComparisonResult(NSString *str1, NSString *str2) {
+            return [str2 compare:str1];
+        }];
+        return @[numbers, strings];
+    }
+
     return @[];
 }
 
